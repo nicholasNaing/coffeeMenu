fc89820ead5e119e4dc228a4d18e89c25369acbf
Nicholas-L-Naing <htutoonaing221b@gmail.com>
2023-12-08 23:56:14 +0630

    adding scroll animations

--- LandingPage/landingPage.js
+++ LandingPage/landingPage.js
@@ -1,7 +1,32 @@
-document.addEventListener("DOMContentLoaded",function(){
-    let appContainer = document.querySelector(".social_media_container")
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log("entry");
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show")
+        } else {
+            entry.target.classList.remove("show")
+        }
+    });
+},{
+    root:null,
+    rootMargin:"-15% 0px -15% 0px"
+});
+  
+const animateHiddens = document.querySelectorAll(".animateHidden");
+const benefitsScroll = document.querySelectorAll(".beneScroll")
 
-    appContainer.addEventListener("click",function(){
-        this.classList.toggle("active")
-    })
+benefitsScroll.forEach((bene)=>{
+    observer.observe(bene)
 })
+
+// Iterate over each element and tell the observer to observe it
+animateHiddens.forEach((element) => {
+    observer.observe(element);
+});
+
+let appContainer = document.querySelector(".social_media_container");
+
+appContainer.addEventListener("click", function () {
+this.classList.toggle("active");
+});
+  
