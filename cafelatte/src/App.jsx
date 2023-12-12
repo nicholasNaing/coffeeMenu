c88e16d9b673e93ae9808af65dedb046ae8bb70a
Nicholas-L-Naing <htutoonaing221b@gmail.com>
2023-12-12 07:22:48 +0630

    created app with react+vite+tailwinds

---
+++ cafelatte/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <div className={`text-slate-500 text-lg font-bold`}>This is the first step of coffee authentication system</div>
+    </>
+  )
+}
+
+export default App
